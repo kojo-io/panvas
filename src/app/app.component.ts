a39851c9303a7db0e0c95d4cae66c1124b2961bd
Bernard Kojo Ossei Agyei <macdee13694@gmail.com>
2023-05-24 06:55:18 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import Compressor from 'compressorjs';
 import {PortalModule} from "@angular/cdk/portal";
 import {ShapesContainerComponent} from "./shapes-container/shapes-container.component";
 import {ShapesEnum} from "./shapes-enum";
+import { PiButtonModule } from "toll-ui/src/public-api";
 
 declare var require: any;
 
@@ -29,7 +30,7 @@ export interface ElementInfo
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, TextFieldComponent, NgIf, FormsModule, PortalModule, ShapesContainerComponent]
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, TextFieldComponent, NgIf, FormsModule, PortalModule, ShapesContainerComponent, PiButtonModule]
 })
 export class AppComponent implements OnInit, AfterViewInit{
   title = 'panvas';
@@ -40,6 +41,9 @@ export class AppComponent implements OnInit, AfterViewInit{
   resizeEast !: HTMLDivElement;
   resizeWest !: HTMLDivElement;
   resizeSouthEast!: HTMLDivElement;
+  resizeSouthWest!: HTMLDivElement;
+  resizeNorthEast!: HTMLDivElement;
+  resizeNorthWest!: HTMLDivElement;
   container = document.createElement('div');
 
   showPortal = false;
@@ -55,7 +59,10 @@ export class AppComponent implements OnInit, AfterViewInit{
     this.body = document.querySelector('.drag-box') as HTMLDivElement;
     this.resizeEast = document.querySelector('.resize-node-e') as HTMLDivElement;
     this.resizeWest = document.querySelector('.resize-node-w') as HTMLDivElement;
-    this.resizeSouthEast =document.querySelector('.resize-node-se') as HTMLDivElement;
+    this.resizeSouthEast = document.querySelector('.resize-node-se') as HTMLDivElement;
+    this.resizeSouthWest = document.querySelector('.resize-node-sw') as HTMLDivElement;
+    this.resizeNorthEast = document.querySelector('.resize-node-ne') as HTMLDivElement;
+    this.resizeNorthWest = document.querySelector('.resize-node-nw') as HTMLDivElement;
   }
 
   createText = () => {
@@ -140,14 +147,11 @@ export class AppComponent implements OnInit, AfterViewInit{
     circle.classList.add('rounded-full', 'outline', 'outline-black', 'outline-2');
     circle.style.width = '50px';
     circle.style.height = '50px';
-    container.draggable = true;
     circle.classList.add('pointer-events-none');
     container.appendChild(circle);
     this.body?.appendChild(container);
 
-    if (this.resizeSouthEast) {
-      container.appendChild(this.resizeSouthEast)
-    }
+    this.appendAllHandles(container);
 
     this.elements.push({
       id: container.id, ele: container, content: '',
@@ -173,9 +177,7 @@ export class AppComponent implements OnInit, AfterViewInit{
       event.preventDefault();
       this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
 
-      if (this.resizeSouthEast) {
-        container.appendChild(this.resizeSouthEast)
-      }
+      this.appendAllHandles(container);
 
       this.showPortal = false;
       this.showTextEdit = false;
@@ -205,30 +207,8 @@ export class AppComponent implements OnInit, AfterViewInit{
     square.classList.add('pointer-events-none');
     this.container.appendChild(square);
     this.body?.appendChild(this.container);
-    setTimeout(() => {
-      this.attachShapeEvent(this.container.id);
-    }, 50)
 
-    if (this.resizeSouthEast) {
-      this.container.appendChild(this.resizeSouthEast)
-    }
-
-    this.container.onclick = (event) => {
-      event.preventDefault();
-      this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
-
-      if (this.resizeSouthEast) {
-        this.container.appendChild(this.resizeSouthEast)
-      }
-
-      this.container.classList.add('border', 'border-dashed', 'border-black')
-      this.selectedElement = {
-        id: this.container.id, ele: this.container,
-        content: '', posX: this.container.getBoundingClientRect().x, posY: this.container.getBoundingClientRect().y ,
-        height: this.container.clientHeight, width: this.container.clientWidth,
-        type: this.elements.find(u => u.id === this.container.id)?.type
-      };
-    }
+    this.appendAllHandles(this.container);
 
     this.elements.push({
       id: this.container.id, ele: this.container, content: '',
@@ -236,6 +216,100 @@ export class AppComponent implements OnInit, AfterViewInit{
       height: this.container.clientHeight, width: this.container.clientWidth,
       type: "shape"
     });
+
+    this.selectedElement = {
+      id: this.container.id, ele: this.container,
+      content: '', posX: this.container.getBoundingClientRect().x, posY: this.container.getBoundingClientRect().y ,
+      height: this.container.clientHeight, width: this.container.clientWidth,
+      resize: this.elements.find(u => u.id === this.container.id)?.resize,
+      type: this.elements.find(u => u.id === this.container.id)?.type
+    };
+
+    this.makeShapeResizable();
+    this.showPortal = false;
+    this.showTextEdit = false;
+    this.showShapeEditor = true;
+
+    this.container.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
+
+        if (this.resizeSouthEast) {
+          this.container.appendChild(this.resizeSouthEast)
+        }
+
+        this.container.classList.add('border', 'border-dashed', 'border-black')
+        this.selectedElement = {
+          id: this.container.id, ele: this.container,
+          content: '', posX: this.container.getBoundingClientRect().x, posY: this.container.getBoundingClientRect().y,
+          height: this.container.clientHeight, width: this.container.clientWidth,
+          resize: this.elements.find(u => u.id === this.container.id)?.resize,
+          type: this.elements.find(u => u.id === this.container.id)?.type
+        };
+      }
+    )
+  }
+
+  onImageChange(file: any): void {
+    this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
+
+    const container = document.createElement('div');
+    container.classList.add('resizable', 'border', 'border-dashed', 'border-black', 'cursor-move', 'absolute', 'flex', 'items-center');
+    container.id = crypto.randomUUID();
+
+
+    const image = document.createElement('img');
+    image.width = 100;
+    image.classList.add('pointer-events-none');
+    container.appendChild(image);
+    this.body?.appendChild(container);
+
+    this.appendAllHandles(container);
+
+    this.elements.push({
+      id: container.id, ele: container, content: image.src,
+      posX: container.getBoundingClientRect().x, posY: container.getBoundingClientRect().y,
+      height: container.clientHeight, width: container.clientWidth,
+      type: "image"
+    });
+
+    this.selectedElement = {
+      id: container.id, ele: container,
+      content: image.src, posX: container.getBoundingClientRect().x, posY: container.getBoundingClientRect().y ,
+      height: container.clientHeight, width: container.clientWidth,
+      resize: this.elements.find(u => u.id === container.id)?.resize,
+      type: this.elements.find(u => u.id === container.id)?.type
+    };
+
+    this.makeImageResizable();
+    this.showPortal = false;
+
+    container.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
+      this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
+
+      this.appendAllHandles(container);
+
+      container.classList.add('border', 'border-dashed', 'border-black')
+      this.selectedElement = {
+        id: container.id, ele: container,
+        content: image.src, posX: container.getBoundingClientRect().x, posY: container.getBoundingClientRect().y ,
+        height: container.clientHeight, width: container.clientWidth,
+        resize: this.elements.find(u => u.id === container.id)?.resize,
+        type: this.elements.find(u => u.id === container.id)?.type
+      };
+
+    }, false)
+
+    if (file.target.files && file.target.files[0]) {
+      new Compressor(file.target.files[0], {
+        quality: 0.6,
+        success(result: File | Blob) {
+          image.src = URL.createObjectURL(result);
+        }
+      });
+    }
   }
 
   ngAfterViewInit() {
@@ -270,7 +344,7 @@ export class AppComponent implements OnInit, AfterViewInit{
   attachImageEvent = (ele: any): any => {
     const p = document.getElementById(ele);
     let resize = new Resizable(p, {
-      handles: 'se',
+      handles: 'se, ne, sw, nw',
       threshold: 10,
       draggable: false
     })
@@ -297,7 +371,7 @@ export class AppComponent implements OnInit, AfterViewInit{
   attachShapeEvent = (ele: any): any => {
     const p = document.getElementById(ele);
     let resize = new Resizable(p, {
-      handles: 'se',
+      handles: 'se, ne, sw, nw',
       threshold: 10,
       draggable: false
     })
@@ -337,76 +411,6 @@ export class AppComponent implements OnInit, AfterViewInit{
   }
 
   selectBackground = () => {}
-
-  onImageChange(file: any): void {
-    this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
-
-    const container = document.createElement('div');
-    container.classList.add('resizable', 'border', 'border-dashed', 'border-black', 'cursor-move', 'absolute', 'flex', 'items-center');
-    container.id = crypto.randomUUID();
-
-
-    const image = document.createElement('img');
-    image.width = 100;
-    image.classList.add('pointer-events-none');
-    container.appendChild(image);
-    this.body?.appendChild(container);
-
-    if (this.resizeSouthEast) {
-      container.appendChild(this.resizeSouthEast);
-    }
-
-    this.elements.push({
-      id: container.id, ele: container, content: image.src,
-      posX: container.getBoundingClientRect().x, posY: container.getBoundingClientRect().y,
-      height: container.clientHeight, width: container.clientWidth,
-      resize: this.attachImageEvent(container.id),
-      type: "image"
-    });
-
-    this.selectedElement = {
-      id: container.id, ele: container,
-      content: image.src, posX: container.getBoundingClientRect().x, posY: container.getBoundingClientRect().y ,
-      height: container.clientHeight, width: container.clientWidth,
-      resize: this.elements.find(u => u.id === container.id)?.resize,
-      type: this.elements.find(u => u.id === container.id)?.type
-    };
-
-    this.makeImageResizable();
-    this.showPortal = false;
-
-    container.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
-      this.unSelectItems(this.resizeEast, this.resizeWest, this.resizeSouthEast);
-
-      if (this.resizeSouthEast) {
-        container.appendChild(this.resizeSouthEast)
-      }
-
-      container.classList.add('border', 'border-dashed', 'border-black')
-      this.selectedElement = {
-        id: container.id, ele: container,
-        content: image.src, posX: container.getBoundingClientRect().x, posY: container.getBoundingClientRect().y ,
-        height: container.clientHeight, width: container.clientWidth,
-        resize: this.elements.find(u => u.id === container.id)?.resize,
-        type: this.elements.find(u => u.id === container.id)?.type
-      };
-
-    }, false)
-
-
-
-
-    if (file.target.files && file.target.files[0]) {
-      new Compressor(file.target.files[0], {
-        quality: 0.6,
-        success(result: File | Blob) {
-          image.src = URL.createObjectURL(result);
-        }
-      });
-    }
-  }
 
   unSelectItems = (resizeEast: any, resizeWest: any, resizeSouthEast: any) => {
     console.log(this.elements.length);
@@ -530,7 +534,7 @@ export class AppComponent implements OnInit, AfterViewInit{
 
       if (findElement) {
         this.selectedElement.resize.destroy();
-
+        console.log(this.selectedElement.resize);
         const ele = (findElement as ElementInfo);
         ele.ele.classList.add('cursor-move');
 
@@ -558,6 +562,27 @@ export class AppComponent implements OnInit, AfterViewInit{
       if (this.selectedElement.type === 'shape') {
         this.makeShapeResizable();
       }
+      if (this.selectedElement.type === 'image') {
+        this.makeImageResizable();
+      }
+    }
+  }
+
+  appendAllHandles = (container: HTMLDivElement) => {
+    if (this.resizeSouthEast) {
+      container.appendChild(this.resizeSouthEast)
+    }
+
+    if (this.resizeNorthWest) {
+      container.appendChild(this.resizeNorthWest)
+    }
+
+    if (this.resizeSouthWest) {
+      container.appendChild(this.resizeSouthWest)
+    }
+
+    if (this.resizeNorthEast) {
+      container.appendChild(this.resizeNorthEast)
     }
   }
 }
